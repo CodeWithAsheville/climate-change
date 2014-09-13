90dcd186a6769e1d3a8720025c504be6e1dc9bda
Paul <paul@pogodan.com>
2014-09-13 15:49:08 -0400

    working anomoly/present stuff

--- js/app.js
+++ js/app.js
@@ -20,19 +20,31 @@ myApp.controller('HomeController', function ($scope, $http) {
     var url_min = "http://www.corsproxy.com/climatedataapi.worldbank.org/climateweb/rest/v1/basin/mavg/ensemble/a2/50/tmin_means/" + $scope.opts.future_start + "/" + $scope.opts.future_end + "/" + $scope.opts.basin_id + ".JSON";
     var url_max = "http://www.corsproxy.com/climatedataapi.worldbank.org/climateweb/rest/v1/basin/mavg/ensemble/a2/50/tmax_means/" + $scope.opts.future_start + "/" + $scope.opts.future_end + "/" + $scope.opts.basin_id + ".JSON";
     
-    // var url_anom = "http://www.corsproxy.com/climatedataapi.worldbank.org/climateweb/rest/v1/basin/mavg/ensemble/a2/50/manom/" + $scope.opts.future_start + "/" + $scope.opts.future_end + "/" + $scope.opts.basin_id + ".JSON";
+    var url_amin = "http://www.corsproxy.com/climatedataapi.worldbank.org/climateweb/rest/v1/basin/manom/ensemble/a2/50/tmin_means/" + $scope.opts.future_start + "/" + $scope.opts.future_end + "/" + $scope.opts.basin_id + ".JSON";
+
+    var url_amax = "http://www.corsproxy.com/climatedataapi.worldbank.org/climateweb/rest/v1/basin/manom/ensemble/a2/50/tmax_means/" + $scope.opts.future_start + "/" + $scope.opts.future_end + "/" + $scope.opts.basin_id + ".JSON";
     
     $http.get(url_min).
       success(function(data, status, headers, config) {
         console.log(data);
         $scope.future.avg_low = round(data[0].monthVals[$scope.opts.month]);
-        $scope.present.avg_low = round(data[0].monthVals[$scope.opts.month] - 3);
+        // $scope.present.avg_low = round(data[0].monthVals[$scope.opts.month] - 3);
+        $http.get(url_amax).
+          success(function(data, status, headers, config) {
+            console.log(data);
+            $scope.present.avg_high = round($scope.future.avg_high - data[0].monthVals[$scope.opts.month]);
+          });
       });
     $http.get(url_max).
       success(function(data, status, headers, config) {
         console.log(data);
         $scope.future.avg_high = round(data[0].monthVals[$scope.opts.month]);
-        $scope.present.avg_high = round(data[0].monthVals[$scope.opts.month] - 3);
+        // $scope.present.avg_high = round(data[0].monthVals[$scope.opts.month] - 3);
+        $http.get(url_amin).
+          success(function(data, status, headers, config) {
+            console.log(data);
+            $scope.present.avg_low = round($scope.future.avg_low - data[0].monthVals[$scope.opts.month]);
+          });
       });
   }
     
