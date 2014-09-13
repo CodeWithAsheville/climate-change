67fccb1e2673208dacfc687cad7fb3ae13cfbccb
Paul <paul@pogodan.com>
2014-09-13 15:04:07 -0400

    updates

--- js/app.js
+++ js/app.js
@@ -13,25 +13,25 @@ myApp.controller('HomeController', function ($scope, $http) {
   };
   
   round = function(n){
-    Math.round(n * 100) / 100;
+    return Math.round(n * 100) / 100;
   }
   
   $scope.load_json = function(){
     var url_min = "http://www.corsproxy.com/climatedataapi.worldbank.org/climateweb/rest/v1/basin/mavg/ensemble/a2/50/tmin_means/" + $scope.opts.future_start + "/" + $scope.opts.future_end + "/" + $scope.opts.basin_id + ".JSON";
     var url_max = "http://www.corsproxy.com/climatedataapi.worldbank.org/climateweb/rest/v1/basin/mavg/ensemble/a2/50/tmax_means/" + $scope.opts.future_start + "/" + $scope.opts.future_end + "/" + $scope.opts.basin_id + ".JSON";
     
+    // var url_anom = "http://www.corsproxy.com/climatedataapi.worldbank.org/climateweb/rest/v1/basin/mavg/ensemble/a2/50/manom/" + $scope.opts.future_start + "/" + $scope.opts.future_end + "/" + $scope.opts.basin_id + ".JSON";
+    
     $http.get(url_min).
       success(function(data, status, headers, config) {
-      console.log(data);
-      $scope.future.avg_low = Math.round(data[0].monthVals[$scope.opts.month] * 100) / 100;
-      $scope.present.avg_low = Math.round((data[0].monthVals[$scope.opts.month] - 10) * 100) / 100;
-    });
+        console.log(data);
+        $scope.future.avg_low = round(data[0].monthVals[$scope.opts.month]);
+      });
     $http.get(url_max).
       success(function(data, status, headers, config) {
-      console.log(data);
-      $scope.future.avg_high = Math.round(data[0].monthVals[$scope.opts.month] * 100) / 100;
-      $scope.present.avg_high = Math.round((data[0].monthVals[$scope.opts.month] - 10) * 100) / 100;
-    });
+        console.log(data);
+        $scope.future.avg_high = round(data[0].monthVals[$scope.opts.month]);
+      });
   }
     
   $(".user-address").geocomplete()
